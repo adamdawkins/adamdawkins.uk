4631aa1611381e7c1e19536fd369d212528055de
Adam Dawkins <adam@dragondrop.uk>
2018-01-03 00:10:25 +0000

    Update homepage

--- imports/ui/pages/Home/Home.js
+++ imports/ui/pages/Home/Home.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import { compose } from 'recompose'
 
-import { logProps } from '../../helpers'
-
 const Home = () => (
-	<div>
-		<p className="h-card">
-			<a className="p-name u-url" href="https://adamdawkins.uk">Adam Dawkins</a>
-		</p>
-		<p>Web Developer at <a href="http://www.dragondrop.uk">Dragon Drop</a>. Thoughts on <a href="https://medium.com/@adamdawkins">other things too</a>.</p>
+	<div className="h-card">
+		<h1><a className="p-name u-url" href="https://adamdawkins.uk">Adam Dawkins</a></h1>
+		<p className="p-note">Web Developer at <a className="org url fn" href="http://www.dragondrop.uk">Dragon Drop</a>. Thoughts on <a href="https://medium.com/@adamdawkins" data-external="true">other things too</a>.</p>
+		<p>Email me at <a className="u-email" href="mailto:adam@dragondrop.uk" rel="me">adam@dragondrop.uk</a></p>
 	</div>
 )
 
 export default compose(
-	logProps('Home', false),
 )(Home)
