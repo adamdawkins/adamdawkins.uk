7c446e4672e8a22ebabad55f74222dbfabbb8e04
Adam Dawkins <adam@dragondrop.uk>
2018-01-03 12:47:38 +0000

    Tidy up

--- imports/startup/server/express-ssr-handler.js
+++ imports/startup/server/express-ssr-handler.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Express from 'express'
 import 'isomorphic-fetch'
 import { StaticRouter } from 'react-router'
-import ReactDOM from 'react-dom/server'
 import { ApolloProvider, renderToStringWithData } from 'react-apollo'
 
 import { Meteor } from 'meteor/meteor'
@@ -10,18 +9,6 @@ import { WebApp } from 'meteor/webapp'
 
 import apolloClient from '../../apollo/client'
 import Routes from '../routes'
-
-function Body({ content, state }) {
-	console.log({content, state})
-	return (
-		<body>
-			<div id="content" dangerouslySetInnerHTML={{ __html: content }} />
-			<script dangerouslySetInnerHTML={{
-				__html: `window.__APOLLO_STATE__=${JSON.stringify(state).replace(/</g, '\\u003c')};`,
-			}} />
-		</body>
-	);
-}
 
 const app = Express()
 app.use((req, res, next) => {
@@ -34,23 +21,20 @@ app.use((req, res, next) => {
 			</StaticRouter>
 		</ApolloProvider>
 	)
-	renderToStringWithData(
-		App
-	).then((content) => {
-		if (context.url) {
-			// Somewhere a `<Redirect>` was rendered
-			res.redirect(301, context.url)
-			next()
 
-		} else {
+	renderToStringWithData(App)
+		.then((content) => {
+			if (context.url) {
+				// Somewhere a `<Redirect>` was rendered
+				res.redirect(301, context.url)
+				next()
+			} else {
+				const initialState = client.extract()
+				const hydrateApollo = `<script>window.__APOLLO_STATE__=${JSON.stringify(initialState).replace(/</g, '\\u003c')};</script>`
+				req.dynamicBody = `<div id="root">${content}</div> ${hydrateApollo}`
 
-			const initialState = client.extract()
-			const hydrateApollo = `<script>window.__APOLLO_STATE__=${JSON.stringify(initialState).replace(/</g, '\\u003c')};</script>`
-			req.dynamicBody = `<div id="root" style="background: yellow">${content}</div> ${hydrateApollo}`
-
-			next()
-		}
-	})
-
+				next()
+			}
+		})
 })
 WebApp.connectHandlers.use(Meteor.bindEnvironment(app))
