684514890bb2e72acaffc93eb5b0893a20df8576
Adam Dawkins <adam@dragondrop.uk>
2018-01-03 20:09:24 +0000

    Display activity counts on notes list

---
+++ imports/ui/components/ActivitySummary.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const ActivitySummary = ({ activities }) => (
+	<div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-start'}}>
+		{activities.map(activity => <ActivityCount key={activity.type} {...activity} />)}
+	</div>
+)
+
+const iconForType = (type) => {
+	switch (type) {
+		case 'repost':
+			return 'fas fa-retweet'
+		case 'like':
+			return 'far fa-heart'
+		case 'reply':
+			return 'fas fa-reply'
+		case 'mention':
+			return 'far fa-comment'
+		default:
+			return 'far fa-comment-alt'
+	}
+}
+
+const ActivityCount = ({ type, count }) => (
+	<div style={{ margin: '0 1em', fontSize: '1.2em' }} title={`${type}s`}>
+		<i className={`${iconForType(type)} fa-pull-left`} />
+		<span>{count}</span>
+	</div>
+)
+
+export default ActivitySummary
--- imports/ui/components/NoteListItem.js
+++ imports/ui/components/NoteListItem.js
@@ -6,12 +6,14 @@ import Author from './Author'
 import SyndicationLinks from './SyndicationLinks'
 import NoteContent from './NoteContent'
 import PublishedAt from './PublishedAt'
+import ActivitySummary from './ActivitySummary'
 
 const NoteListItem = ({ note, showAuthor }) => (
 	<article className="h-entry" style={{borderBottom: '1px solid black', paddingBottom: '2em'}}>
 		{showAuthor && <Author />}
 		<NoteContent note={note} />
 		<footer>
+			<ActivitySummary activities={note._activityMeta} />
 			{note.publishedAt && <PublishedAt note={note} /> }
 			<SyndicationLinks note={note} />
 		</footer>
