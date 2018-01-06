058a3c9568159a960a274e0d30f7614e93d16b3e
Adam Dawkins <adam@dragondrop.uk>
2018-01-06 21:11:47 +0000

    Remove activity counts from note list for performance

--- imports/ui/components/NoteListItem.js
+++ imports/ui/components/NoteListItem.js
@@ -6,14 +6,12 @@ import Author from './Author'
 import SyndicationLinks from './SyndicationLinks'
 import NoteContent from './NoteContent'
 import PublishedAt from './PublishedAt'
-import ActivitySummary from './ActivitySummary'
 
 const NoteListItem = ({ note, showAuthor }) => (
 	<article className="h-entry" style={{borderBottom: '1px solid black', paddingBottom: '2em'}}>
 		{showAuthor && <Author />}
 		<NoteContent note={note} />
 		<footer>
-			<ActivitySummary activities={note._activityMeta} />
 			<PublishedAt url={note.url} date={note.publishedAt} />
 			<SyndicationLinks note={note} />
 		</footer>
