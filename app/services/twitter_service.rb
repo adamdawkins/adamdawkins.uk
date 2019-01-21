dce024ad7d7cf02e24c43c433c31801cf993e150
Adam Dawkins <adam@dragondrop.uk>
2019-01-21 19:18:43 +0000

    ✅ Truncate Tweet with link to original on long notes

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -5,10 +5,20 @@ TwitterClient = Twitter::REST::Client.new do |config|
   config.access_token_secret = Rails.application.credentials.twitter[:access_token_secret]
 end
 
+MAX_TWEET_LENGTH = 280
+
 class TwitterService
   def self.post(post)
     tweet = post.content
     tweet.gsub!('*', '＊')
+    if (tweet.length > MAX_TWEET_LENGTH)
+      pp "too long"
+      pp post.id
+      url = "#{ENV['SHORT_URL']}/#{post.id}"
+      pp url
+      cut_off = MAX_TWEET_LENGTH - url.length - 4 # the 4 is 3 elipses dots + a space or return
+      tweet = "#{tweet[0..cut_off]}...\r#{url}"
+    end
     response = TwitterClient.update(tweet)
 
     twitter_silo = Silo.find_by(name: "Twitter")
