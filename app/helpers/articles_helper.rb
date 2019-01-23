68594e133ee34ef337e753f483d8ec782949f426
Adam Dawkins <adam@dragondrop.uk>
2019-01-23 23:18:18 +0000

    🐞 Fix error where article does not contain a paragraph

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,8 @@
 module ArticlesHelper
   def blurb(article, length: 280)
     html = Kramdown::Document.new(article.content).to_html.html_safe
-    first_paragraph = Nokogiri::HTML(html).css('p').first.text()
-    simple_format first_paragraph
+    first_p = Nokogiri::HTML(html).css('p').first
+
+    simple_format first_p.text() if first_p.present?
   end
 end
