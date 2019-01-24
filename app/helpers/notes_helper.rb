56e31a1df8128c9bed457af9a3e03cd17b6763c0
Adam Dawkins <adam@dragondrop.uk>
2019-01-24 21:23:37 +0000

    🐞 Output line breaks in Notes

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,5 +1,11 @@
 module NotesHelper
   def note_content(note)
-    auto_link(note.content)
+    line_breaks(
+      auto_link(note.content)
+    )
+  end
+
+  def line_breaks(text)
+    text.gsub(/(\r\n)/, '<br/>').html_safe
   end
 end
