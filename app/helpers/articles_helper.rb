702d73a4eb45c2c5424ee1ac8eec08122551faac
Adam Dawkins <adam@dragondrop.uk>
2019-02-06 21:04:51 +0000

    ♻️  Move HTML render methods for Notes/Articles to models...
    ... I may regret this, but we use the HTML view to determine things like
    whether or not there's a link preview, so having it on the model is
    helpful

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,16 +1,2 @@
 module ArticlesHelper
-  ArticlePipeline = HTML::Pipeline.new [
-    HTML::Pipeline::MarkdownFilter,
-  ]
-
-  def blurb(article, length: 280)
-    html = article_content(article.content)
-    first_p = Nokogiri::HTML(html).css('p').first
-
-    simple_format first_p.text() if first_p.present?
-  end
-
-  def article_content(content)
-    ArticlePipeline.to_html(content).html_safe
-  end
 end
--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,25 +1,4 @@
-require 'html/pipeline'
-context = {
-  base_url: "https://twitter.com",
-  username_pattern: /[a-z0-9][a-z0-9_]*/i,
-  tag_url: 'https://twitter.com/hashtag/%{tag}'
-}
-
-
-NotePipeline = HTML::Pipeline.new [
-  HTML::Pipeline::MentionFilter,
-  HTML::Pipeline::AutolinkFilter,
-  HTML::Pipeline::HashtagFilter,
-  HTML::Pipeline::LineBreakFilter,
-], context
-
-
-
 module NotesHelper
-  def note_content(note)
-      html = NotePipeline.to_html(note.content).html_safe
-  end
-
   def reply_text(link)
     tweet = link.match(/twitter.com\/(\w*)\//)
     return link if tweet.nil?
