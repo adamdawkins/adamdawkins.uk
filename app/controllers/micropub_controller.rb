b3915dc88f38295592b1aeb4f347270bfae4e3d1
Adam Dawkins <adam@dragondrop.uk>
2019-09-26 23:11:19 +0100

    ✅  Micropub: 100: Create an h-entry post (form-encoded)

--- app/controllers/micropub_controller.rb
+++ app/controllers/micropub_controller.rb
@@ -1,4 +1,8 @@
 class MicropubController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
+    @note = Note.create(content: params[:content])
+    redirect_to long_post_path(@note.params), status: :created if @note.publish!
   end
 end
--- spec/requests/micropub_requests_spec.rb
+++ spec/requests/micropub_requests_spec.rb
@@ -4,11 +4,10 @@ require 'rails_helper'
 RSpec.describe "Micropub Requests", type: :request do
   describe "POST /micropub" do
     it "Creates a basic form-encoded h-entry post" do
-      pp micropub_url
     post micropub_url, params: { h: 'entry', content: 'Hello world' }
-    pp response.headers
+
     expect(response).to have_http_status :created
-    expect(response.headers["Location"]).to match /Hello-world/
+    expect(response.headers["Location"]).to match /hello-world/
     end
 
   end
