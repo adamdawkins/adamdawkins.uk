93a95ee91316a7577407b9d692092aa9d984de64
Adam Dawkins <adam@dragondrop.uk>
2019-09-26 23:25:28 +0100

    ✅ Micropub: 200: Create an h-entry post (JSON)

--- app/controllers/micropub_controller.rb
+++ app/controllers/micropub_controller.rb
@@ -2,7 +2,16 @@ class MicropubController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    @note = Note.create(content: params[:content])
+    if request.content_type == 'application/json'
+      create_note_from_json_params
+    else
+      @note = Note.create(content: params[:content])
+    end
     redirect_to long_post_path(@note.params), status: :created if @note.publish!
   end
+
+  private
+    def create_note_from_json_params
+      @note = Note.create(content: params[:properties][:content])
+    end
 end
--- spec/requests/micropub_requests_spec.rb
+++ spec/requests/micropub_requests_spec.rb
@@ -3,12 +3,19 @@ require 'rails_helper'
 
 RSpec.describe "Micropub Requests", type: :request do
   describe "POST /micropub" do
-    it "Creates a basic form-encoded h-entry post" do
-    post micropub_url, params: { h: 'entry', content: 'Hello world' }
+    it "100: Creates a basic form-encoded h-entry post" do
+      post micropub_url, params: { h: 'entry', content: 'Hello world' }
 
-    expect(response).to have_http_status :created
-    expect(response.headers["Location"]).to match /hello-world/
+      expect(response).to have_http_status :created
+      expect(response.headers["Location"]).to match /hello-world/
     end
 
+    it "200: Create an h-entry post (JSON)" do
+      headers = { "CONTENT_TYPE" => "application/json" }
+      post micropub_url, params: '{ "type": ["h-entry"], "properties": { "content": ["Micropub test of creating an h-entry with a JSON request"] } }', headers: headers
+
+      expect(response).to have_http_status :created
+      expect(response.headers["Location"]).to match /micropub-test/
+    end
   end
 end
