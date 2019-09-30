ef8e184ac50a339e7fcdb9d208a4665b8e0dd4a6
Adam Dawkins <adam@dragondrop.uk>
2019-09-30 23:44:28 +0100

    ✅ Add Micropub authentication and basic posts

--- app/controllers/micropub_controller.rb
+++ app/controllers/micropub_controller.rb
@@ -1,6 +1,8 @@
 class MicropubController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  before_action :authorize_request
+
   def create
     if request.content_type == 'application/json'
       create_note_from_json_params
@@ -11,7 +13,25 @@ class MicropubController < ApplicationController
   end
 
   private
-    def create_note_from_json_params
-      @note = Note.create(content: params[:properties][:content])
-    end
+
+  def create_note_from_json_params
+    @note = Note.create(content: params[:properties][:content])
+  end
+
+  def authorize_request
+    token = request.headers['Authorization']
+
+    render plain: '401 Not authorized', status: :unauthorized and return if
+      token.nil?
+
+    response = HTTParty.get('https://tokens.indieauth.com/token',
+                            headers: { 'Authorization' => token })
+    indieauth_params = Rack::Utils.parse_nested_query(
+      CGI.unescape(response.body)
+    )
+
+    return if URI.parse(indieauth_params['me']).host == ENV['FULL_URL']
+
+    render plain: '401 Not authorized', status: :unauthorized
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,6 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
-
 VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock
